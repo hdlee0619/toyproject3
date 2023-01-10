c34eed394d523937513666aab26c9456a510daa4
hdlee0619 <dlgusehd1211@naver.com>
2023-01-10 22:47:03 +0900

    네이버 크롤링 기능 삭제 - 네이버 크롤링 기능이 작동하지 않아 일단 삭제

--- app.py
+++ app.py
@@ -44,28 +44,13 @@ def blog_post():
     
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
-    
+    data = requests.get(vlog_url_receive, headers=headers)
 
     soup = BeautifulSoup(data.text, 'html.parser')
-    
-    naver_url = vlog_url_receive
 
-    test = naver_url.split('.')
-
-    if test[1] == "naver":
-      naver_url_iframe = soup.select_one('iframe#mainFrame')['src']
-      naver_blog_url = "http://blog.naver.com" + naver_url_iframe
-      
-      data = requests.get(naver_blog_url, headers=headers)
-
-      title_receive = soup.select_one('meta[property="og:title"]')['content']
-      img_receive = soup.select_one('meta[property="og:image"]')['content']
-      desc_receive = soup.select_one('meta[property="og:description"]')['content']
-    else:
-      data = requests.get(vlog_url_receive, headers=headers)
-      title_receive = soup.select_one('meta[property="og:title"]')['content']
-      img_receive = soup.select_one('meta[property="og:image"]')['content']
-      desc_receive = soup.select_one('meta[property="og:description"]')['content']
+    title_receive = soup.select_one('meta[property="og:title"]')['content']
+    img_receive = soup.select_one('meta[property="og:image"]')['content']
+    desc_receive = soup.select_one('meta[property="og:description"]')['content']
 
     doc = {
         'name': name_receive,
@@ -90,4 +75,4 @@ def like():
     return jsonify({'msg': '좋아요 완료!'})
 
 if __name__ == '__main__':
-    app.run('0.0.0.0', port=5003, debug=True)
+    app.run('0.0.0.0', port=5000, debug=True)
