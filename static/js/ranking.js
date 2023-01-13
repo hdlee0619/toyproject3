c9e50b336e18508caca732bc7ee08c5c9728114f
HEUKWU <yoo4068@gmail.com>
2023-01-13 21:52:27 +0900

    랭킹 함수 수정

--- static/js/ranking.js
+++ static/js/ranking.js
@@ -9,7 +9,6 @@ function listing() {
       data: {},
       success: function (response) {
          let rows = response["tils"];
-         let rank = Array.from(Array(rows.length), () => new Array(3));
          for (let i = 0; i < rows.length; i++) {
             let like = rows[i]["like"];
             let name = rows[i]["name"];
@@ -17,12 +16,13 @@ function listing() {
             let title = rows[i]["title"];
             let img = rows[i]['img']
 
-            rank[i] = [like, name, address, title, img];
+            rows[i] = [like, name, address, title, img];
          }
+         console.log(rows)
 
-         let sortedRank = rank.sort((a, b) => {
+         rows.sort((a, b) => {
             if (a[0] === b[0]) {
-               return a[1] < b[1] ? -1 : a[1] > b[0] ? 1 : 0;
+               return a[1] < b[1] ? -1 : a[1] > b[1] ? 1 : 0;
             } else {
                return b[0] - a[0];
             }
@@ -30,10 +30,10 @@ function listing() {
           // 1등 출력
                ranking1_html = `
                      <div class="rank">
-                    <img src=${sortedRank[0][4]} alt="1등이미지">
-                <h4>${sortedRank[0][3]}</h4>
-               <p>작성자 : ${sortedRank[0][1]}</p>
-               <span>👍: ${sortedRank[0][0]}</span>
+                    <img src=${rows[0][4]} alt="1등이미지">
+                <h4>${rows[0][3]}</h4>
+               <p>작성자 : ${rows[0][1]}</p>
+               <span>👍: ${rows[0][0]}</span>
            </div>
                    `;
                $("#first").append(ranking1_html)
@@ -42,17 +42,17 @@ function listing() {
                   //2,3등 출력
                  ranking2_html = `
                      <div class="rank1">
-                    <img src=${sortedRank[1][4]} alt="2등이미지">
-                <h4>${sortedRank[1][3]}</h4>
-               <p>작성자 : ${sortedRank[1][1]}</p>
-               <span>👍: ${sortedRank[1][0]}</span>
+                    <img src=${rows[1][4]} alt="2등이미지">
+                <h4>${rows[1][3]}</h4>
+               <p>작성자 : ${rows[1][1]}</p>
+               <span>👍: ${rows[1][0]}</span>
            </div>
 
                  <div class="rank1">
-                    <img src=${sortedRank[2][4]} alt="3등이미지">
-                        <h4>${sortedRank[2][3]}</h4>
-                        <p>작성자 : ${sortedRank[2][1]}</p>
-                         <span>👍: ${sortedRank[2][0]}</span>
+                    <img src=${rows[2][4]} alt="3등이미지">
+                        <h4>${rows[2][3]}</h4>
+                        <p>작성자 : ${rows[2][1]}</p>
+                         <span>👍: ${rows[2][0]}</span>
                  </div>
 
                    `;
@@ -63,12 +63,12 @@ function listing() {
 
 
          let count = 0;
-         for (let i = 0; i < sortedRank.length; i++) {
+         for (let i = 0; i < rows.length; i++) {
             count++;
-            let like = sortedRank[i][0];
-            let name = sortedRank[i][1];
-            let address = sortedRank[i][2];
-            let title = sortedRank[i][3];
+            let like = rows[i][0];
+            let name = rows[i][1];
+            let address = rows[i][2];
+            let title = rows[i][3];
 
             let temp_html = `
                <tr>
